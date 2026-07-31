1722ed9b986075a89787931a5ae49d12e83328a5
TejasVKolhe <tejaskolhe0505@gmail.com>
2026-07-30 22:42:01 -0700

    Adding adv 062

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-30T22:30:03-0700",
+    date = "2026-07-30T22:39:41-0700",
     comments = "version: 1.6.2, compiler: javac, environment: Java 25.0.4 (Oracle Corporation)"
 )
 @Component
